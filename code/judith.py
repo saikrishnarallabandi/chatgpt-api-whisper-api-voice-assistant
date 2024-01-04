fe325c9846bdc4767a9c028eee024b5652282a19
saikrishnarallabandi <srallaba.research@gmail.com>
2024-01-04 17:38:18 -0600

    added dir structure

--- code/judith.py
+++ code/judith.py
@@ -1,10 +1,25 @@
 import gradio as gr
 import os
-import openai, config, subprocess
+import openai
 import speech_recognition as sr
 from openai import OpenAI
 from gtts import gTTS
+import random
+import string
+import datetime
 import concurrent.futures
+
+def generate_random_code():
+    code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=16))
+    now = datetime.datetime.now()
+    date = now.strftime("%Y-%m-%d")
+    time = now.strftime("%H:%M:%S")
+    return code, date, time
+
+random_code, current_date, time = generate_random_code()
+exp_name = "judith-gradle-" + current_date + "-" +  time.replace(":", "-") + '-' +  random_code 
+exp_dir = '../exp/' + exp_name
+os.mkdir(exp_dir)
 
 client = OpenAI()
 prompt_audio = 'You are Judith, a personal AI assistant. Your job is to help the user to the best of your abilities \
@@ -12,6 +27,11 @@ prompt_audio = 'You are Judith, a personal AI assistant. Your job is to help the
     Remember to respond in 20 words or less.'
 prompt_text = 'You are Judith, a personal AI assistant. Your job is to help the user to the best of your abilities \
     Figure out what your user needs and try to help them as much as you can. '
+       
+f = open(exp_dir + '/' + 'logfile', 'w')
+f.write(prompt_text + '\n')
+f.close()
+
         
 messages = [{"role": "system", "content": prompt_text}]
 messages_audio = [{"role": "system", "content": prompt_audio}]
@@ -21,6 +41,10 @@ def get_text_response(messages):
     response = client.chat.completions.create(model="gpt-3.5-turbo-1106", messages=messages)
     system_message = response.choices[0].message.content
     messages.append({"role": "system", "content": system_message})
+    f = open(exp_dir + '/' + 'logfile', 'a')
+    f.write('Julia: ' +  system_message + '\n')
+    f.close()
+    
     return system_message, messages
 
 def get_audio_response(messages_audio):
@@ -35,14 +59,14 @@ def transcribe(audio=None, input_data=None):
     global messages, messages_audio
 
     if audio is not None: input_data = None 
-     
+    
     if isinstance(input_data, str):
         # Handle text input
         transcript = input_data
     
     else:    
 
-        audio_filename_with_extension = audio + '.wav'
+        audio_filename_with_extension = exp_dir + '/input_' + str(len(messages_audio)+1).zfill(3) + '.wav'
         os.rename(audio, audio_filename_with_extension)
     
         r = sr.Recognizer()
@@ -50,6 +74,10 @@ def transcribe(audio=None, input_data=None):
             audio_data = r.record(source)
             transcript = r.recognize_google(audio_data)
 
+    f = open(exp_dir + '/' + 'logfile', 'a')
+    f.write('User: ' +  transcript + '\n')
+    f.close()
+ 
     messages.append({"role": "user", "content": transcript})
     messages_audio.append({"role": "user", "content": transcript})
 
@@ -75,9 +103,10 @@ def transcribe(audio=None, input_data=None):
 
             
     tts = gTTS(system_message_audio, lang='en')
-    tts.save('output.mp3')       
+    audio_fname = exp_dir + '/' + 'output_' + str(len(messages_audio)).zfill(3) + '.mp3'
+    tts.save(audio_fname)       
 
-    return chat_transcript, transcript, 'output.mp3'
+    return chat_transcript, transcript, audio_fname
 
 ui = gr.Interface(
     fn=transcribe, 
